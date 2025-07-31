17798a1e82f930efc1df65f7645649f3c1bbbe2c
Harrby <hjb9987@gmail.com>
2025-07-31 18:24:00 +0100

    Remove large build files and apply .gitignore

--- build-NeuralNetViewer-Desktop_x86_windows_msvc2022_pe_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_NeuralNetViewer_FILE Z:/learningqt/NeuralNetViewer/build-NeuralNetViewer-Desktop_x86_windows_msvc2022_pe_64bit-Debug/NeuralNetViewer.exe)
-set(__QT_DEPLOY_TARGET_NeuralNetViewer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_NeuralNetViewer_RUNTIME_DLLS Z:/qtcreator/6.9.1/msvc2022_64/bin/Qt6Widgetsd.dll;Z:/qtcreator/6.9.1/msvc2022_64/bin/Qt6Guid.dll;Z:/qtcreator/6.9.1/msvc2022_64/bin/Qt6Cored.dll)
